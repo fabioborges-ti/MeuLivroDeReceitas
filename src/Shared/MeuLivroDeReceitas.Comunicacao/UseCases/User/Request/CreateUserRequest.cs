2093692bbee40371e468c1611a82609ab9066234
Fábio Nascimento <fabioborges.ti@gmail.com>
2022-11-06 01:49:34 -0300

    . (#4)
    Co-authored-by: Fabio Borges <fabioborges.ti@outlook.com>

---
+++ src/Shared/MeuLivroDeReceitas.Comunicacao/UseCases/User/Request/CreateUserRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MeuLivroDeReceitas.Comunicacao.UseCases.User.Request;
+
+public class CreateUserRequest
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Password { get; set; }
+}
